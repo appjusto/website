59cfb3b2f0ba1e2a04c82fe14505fd426fe372ed
RenanC <renancostam@gmail.com>
2020-11-26 16:48:40 -0300

    Test buttons hover colors 3

--- src/styles/button.ts
+++ src/styles/button.ts
@@ -17,33 +17,6 @@ export default {
       bg: "secondary",
       h: "60px",
       fontSize: "18px",
-      _hover: {bg: "primary", color: "primaryDark", borderColor: "primaryDark"}
-    },
-    secondaryLight: {
-      bg: "secondaryLight",
-      h: "48px",
-      fontSize: "15px",
-      padding: "0",
-      _hover: {bg: "primary", color: "primaryDark", borderColor: "primaryDark"}
-    },
-  },
-  defaultProps: {
-    variants: "white"
-  },
-}
-
-/*
-variants: {
-    white:{
-      bg: "white",
-      fontSize: "15px",
-      h: "48px",
-      _hover: {bg: "#F2F6EA"}
-    },
-    secondary: {
-      bg: "secondary",
-      h: "60px",
-      fontSize: "18px",
       _hover: {bg: "#FF7F22"}
     },
     secondaryLight: {
@@ -54,4 +27,31 @@ variants: {
       _hover: {bg: "#FFC093"}
     },
   },
-  */
+  defaultProps: {
+    variants: "white"
+  },
+}
+
+/*
+ variants: {
+  white:{
+    bg: "white",
+    fontSize: "15px",
+    h: "48px",
+    _hover: {bg: "primary", color: "primaryDark", borderColor: "primaryDark"}
+  },
+  secondary: {
+    bg: "secondary",
+    h: "60px",
+    fontSize: "18px",
+    _hover: {bg: "primary", color: "primaryDark", borderColor: "primaryDark"}
+  },
+  secondaryLight: {
+    bg: "secondaryLight",
+    h: "48px",
+    fontSize: "15px",
+    padding: "0",
+    _hover: {bg: "primary", color: "primaryDark", borderColor: "primaryDark"}
+  },
+},
+*/
