c7dfc10f6ca29f94b2594a1a5c9a96c13aa5a32c
RenanC <renancostam@gmail.com>
2020-12-04 14:36:06 -0300

    Test hero images with green bottom

--- src/components/home/hero/index.tsx
+++ src/components/home/hero/index.tsx
@@ -22,7 +22,7 @@ const Hero: React.FC = () => {
     <Section
       position="relative" 
       id="hero"
-      bgColor="black"
+      bgColor="primary"
       h={["auto", null, null, "560px", "560px", "600px"]} 
     >
       <Container 
