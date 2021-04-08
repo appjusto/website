cd56e93ba617257d5d934b9315d8cc08bd202d30
RenanC <renancostam@gmail.com>
2021-04-08 11:57:45 -0300

    Add compressed images

--- frontend/src/pages/_document.tsx
+++ frontend/src/pages/_document.tsx
@@ -13,7 +13,7 @@ export default class MyDocument extends Document {
       <Html lang="pt-BR">
         <Head>
           <Seo
-            metaDescription="AppJusto. Mais do que um app de entregas. Somo um movimento por relações mais justas e transparentes. Faça parte agora!"
+            metaDescription="Mais do que um app de entregas. Um movimento por relações mais justas e transparentes para restaurantes, entregadores e clientes. Faça parte desse movimento!"
             title="AppJusto"
             author="@appjusto"
             canonical_url="https://appjusto.com.br/"
