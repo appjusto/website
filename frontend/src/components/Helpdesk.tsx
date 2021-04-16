4150b3450fc21aad7e538a4fb86a4669263c717a
RenanC <renancostam@gmail.com>
2021-04-16 10:18:58 -0300

    Hide fresdesck ticketForm fields

--- frontend/src/components/Helpdesk.tsx
+++ frontend/src/components/Helpdesk.tsx
@@ -1,9 +1,11 @@
 import { Box } from "@chakra-ui/layout"
-import React from 'react';
+import React, { useCallback, useRef, useEffect } from 'react';
 
 const Helpdesk: React.FC = () => {
-
-  const initWidget = React.useCallback(() => {
+  // refs
+  const initialized = useRef(false);
+  // handlers
+  const initWidget = useCallback(() => {
     //@ts-ignore
     window.fwSettings={
       'widget_id': 67000002819
@@ -18,20 +20,32 @@ const Helpdesk: React.FC = () => {
       n.q = [],
       //@ts-ignore
       window.FreshworksWidget = n
+      //@ts-ignore
+      window.FreshworksWidget('prefill', 'ticketForm', { subject: 'Landing page' });
+      //@ts-ignore
+      window.FreshworksWidget('hide', 'ticketForm', ['subject','product_id','']);
     }
+    initialized.current = true;
   },[]);
 
-  React.useEffect(() => {
+  // side effects
+  useEffect(() => {
     const script = document.createElement('script');
     script.src = 'https://widget.freshworks.com/widgets/67000002819.js';
     script.type = 'text/javascript';
     document.body.appendChild(script);
   }, []);
 
-  React.useEffect(() => {
+  useEffect(() => {
     initWidget();
   }, [initWidget]);
 
+  /*useEffect(() => {
+      let subjectField = document.getElementById('subject');
+      if(subjectField) subjectField.parentElement.style.visibility = "hidden";
+  }, [])*/
+  console.log("render")
+  // placeholder UI
   return <Box />
 }
 
