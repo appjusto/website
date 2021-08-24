024c94cf61ff2a5755824c262ef648a475ca75b8
RenanC <renancostam@gmail.com>
2021-08-24 19:33:34 -0300

    Remove firestore from firebase.json

--- frontend/src/pages/frota/[fleetId].tsx
+++ frontend/src/pages/frota/[fleetId].tsx
@@ -9,8 +9,31 @@ import Footer from "../../components/Footer";
 import Topic from "../../components/home/better/Topic";
 import { usePageContext } from "../../context";
 import React from 'react';
-import { Fleet } from '@appjusto/types';
+//import { Fleet } from '@appjusto/types';
 import { formatCurrency } from "../../utils";
+
+export type FleetSituation = 'pending' | 'approved' | 'rejected' | 'blocked';
+
+export interface FleetFareParams {
+  minimumFee: number; // (in cents) minimum fee charged by couriers
+  distanceThreshold: number; // (in meters) distanced covered by the minimumFee
+  additionalPerKmAfterThreshold: number; // (in cents) fee charged every km after distanceThreshold
+  maxDistance: number; // (in meters) maximum trip distance
+  maxDistanceToOrigin: number; // (in meters) maximum distance to the pickup place
+}
+
+export interface FleetDetails extends FleetFareParams {
+  name: string;
+  description: string;
+  // metadata
+  createdOn: any;
+  createdBy?: string;
+}
+
+export interface Fleet extends FleetDetails {
+  situation: FleetSituation;
+  participantsOnline: number;
+}
 
 /*export const getStaticPaths: GetStaticPaths = async () => {
   return {
