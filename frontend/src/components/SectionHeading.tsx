f7ab3d61fbb29d710b034eeadcd4554e19fde23c
RenanC <renancostam@gmail.com>
2021-11-25 13:19:54 -0300

    MainVideo responsivity

--- frontend/src/components/SectionHeading.tsx
+++ frontend/src/components/SectionHeading.tsx
@@ -1,13 +1,19 @@
 import { Heading, HeadingProps } from '@chakra-ui/react';
 
-const SectionHeading: React.FC<HeadingProps> = ({ children }) => {
+interface SectionHeadingProps extends HeadingProps {
+  highlighted?: boolean;
+}
+
+const SectionHeading: React.FC<SectionHeadingProps> = ({ highlighted = false, children, ...props }) => {
   return (
     <Heading
+      as={highlighted ? 'h2' : 'h3'}
       color="black"
-      fontSize={{ base: '24px', lg: '32px' }}
+      fontSize={highlighted ? { base: '28px', lg: '48px' } : { base: '24px', lg: '36px' }}
+      lineHeight={highlighted ? { base: '30px', lg: '57px' } : { base: '30px', lg: '43.2px' }}
       fontWeight="700"
-      lineHeight={{ base: '30px', lg: '38.4px' }}
       mb="4"
+      {...props}
     >
       {children}
     </Heading>
--- frontend/src/components/home/main-video/index.tsx
+++ frontend/src/components/home/main-video/index.tsx
@@ -1,34 +1,35 @@
-import { Box, Button, Center, Flex, Heading, Text } from '@chakra-ui/react'
+import { Box, Button, Center, Flex, Text } from '@chakra-ui/react'
 import Image from '../../Image';
 import Section from "../../Section";
 import Container from '../../Container';
 import NextLink from 'next/link';
+import SectionHeading from '../../SectionHeading';
 
 const MainVideo: React.FC = () => {
   return (
-    <Section mt={{ lg: '4'}} id="main-video" h="auto">
-      <Container bgColor="#2F422C" pt="16" px="100px" pb="2" >
+    <Section mt={{ base: '8', lg: '4'}} id="main-video" h="auto">
+      <Container bgColor="#2F422C" pt={{base: '8', lg: '16'}} px={{base: '4', md:'6', lg: '100px'}} pb={{base: '10', lg: '2'}} >
         <Flex direction={{base: 'column', md: 'row'}} h="100%" color="white">
-          <Box w="60%">
+          <Box w={{base: '100%', md: '50%', lg: '60%'}}>
             <Box position="relative">
-              <Image src="/team.jpeg" boxShadow="black -24px 24px" zIndex="100" />
-            <Center position="absolute" top="0" left="0" w="100%" h="100%" zIndex="999">
+              <Image src="/team.jpeg" boxShadow={{lg: 'black -24px 24px'}} zIndex="100" />
+              <Center position="absolute" top="0" left="0" w="100%" h="100%" zIndex="999">
                 <Image src="/icon-play.svg" w="96px" h="96px" _hover={{ opacity: "0.6" }} cursor="pointer" />
               </Center>
             </Box>
-            <Text mt="8" fontSize="16px" lineHeight="22px" fontWeight="500" textAlign="center">
+            <Text mt={{base: '2', lg: '8'}} fontSize="16px" lineHeight="22px" fontWeight="500" textAlign="center">
               Assista ao vídeo e entenda a nossa proposta
             </Text>
           </Box>
-          <Box w="40%" py="4" px="5">
-            <Heading as="h2" fontSize="48px" lineHeight="57.6px" fontWeight="700">
+          <Box mt={{base: '8' , lg: '0'}} w={{base: '100%', md: '50%', lg: '40%'}} py={{ lg: '4'}} px={{ lg: '5'}} pl={{md: '6', lg: '0'}}>
+            <SectionHeading highlighted color="white">
               Só é bom quando é bom para todos
-            </Heading>
-            <Text mt="8" fontSize="20px" lineHeight="26px" fontWeight="500">
+            </SectionHeading>
+            <Text mt="8" fontSize={{base: '18px', lg: '20px'}} lineHeight="26px" fontWeight="500">
               Mais que uma plataforma de entregas. Queremos ser exemplo de como um negócio da economia de plataforma pode crescer e, ao mesmo tempo, gerar prosperidade para todos - fornecedores, consumidores, entregadores, acionistas - reduzindo desigualdades.
             </Text>
             <NextLink  href="/investimento-coletivo" passHref>
-              <Button mt="8" w="auto" variant="outlineWhite" px="6">
+              <Button mt="8" w={{base: '100%', lg: 'auto'}} size="lg" variant="outlineWhite" px="6">
                 Saiba mais do financiamento coletivo
               </Button>
             </NextLink>
