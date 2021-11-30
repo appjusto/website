44d3502354e07a812a47fb50f48ecabfba873270
RenanC <renancostam@gmail.com>
2021-11-30 16:58:58 -0300

    Testimonials adjustments

--- frontend/src/components/home/testimonials/index.tsx
+++ frontend/src/components/home/testimonials/index.tsx
@@ -1,8 +1,44 @@
-import { Box, Center, Image, Stack } from '@chakra-ui/react'
+import { Box, Center, Image, HStack } from '@chakra-ui/react'
 import Section from "../../Section";
 import Container from '../../Container';
 import SectionHeading from '../../SectionHeading';
 import { usePageContext } from '../../../context';
+
+interface TestimonialBoxProps {
+  image: string;
+  handleClick(): void;
+}
+
+const TestimonialBox = ({ image, handleClick }: TestimonialBoxProps) => {
+  return (
+    <Box
+      position="relative"
+      flex={1}
+      minW="312px"
+      borderRadius="16px"
+      overflow="hidden"
+    >
+      <Image src={image} />
+      <Center
+        position="absolute"
+        top="0"
+        left="0"
+        w="100%"
+        h="100%"
+        zIndex="200"
+        _hover={{ opacity: "0.6" }}
+        cursor="pointer"
+        onClick={handleClick}
+      >
+        <Image
+          src="/icon-play.svg"
+          w="96px"
+          h="96px"
+        />
+      </Center>
+    </Box>
+  )
+}
 
 const Testimonials: React.FC = () => {
   // context
@@ -14,95 +50,32 @@ const Testimonials: React.FC = () => {
         <SectionHeading w="100%" textAlign={{ md: 'center'}}>
           Depoimentos de quem já faz parte da mudança
         </SectionHeading>
-        <Stack mt="10" direction={{base: 'column', md: 'row'}} spacing={4} h="100%">
-          <Box
-            position="relative"
-            flex={1}
-            borderRadius="16px"
-            overflow="hidden"
-          >
-            <Image src="/testimonials1.jpeg" />
-            <Center
-              position="absolute"
-              top="0"
-              left="0"
-              w="100%"
-              h="100%"
-              zIndex="200"
-            >
-              <Image
-                src="/icon-play.svg"
-                w="96px"
-                h="96px"
-                _hover={{ opacity: "0.6" }}
-                cursor="pointer"
-                onClick={() => setVideoModalConfig({
-                  isOpen: true,
-                  videoId: '9qK8lmLd3w4',
-                  title: 'Depoimento Jurandir'
-                })}
-              />
-            </Center>
-          </Box>
-          <Box
-            position="relative"
-            flex={1}
-            borderRadius="16px"
-            overflow="hidden"
-          >
-            <Image src="/testimonials2.jpeg" />
-            <Center
-              position="absolute"
-              top="0"
-              left="0"
-              w="100%"
-              h="100%"
-              zIndex="200"
-            >
-              <Image
-                src="/icon-play.svg"
-                w="96px"
-                h="96px"
-                _hover={{ opacity: "0.6" }}
-                cursor="pointer"
-                onClick={() => setVideoModalConfig({
-                  isOpen: true,
-                  videoId: 'y7_B4kCI93o',
-                  title: 'Depoimento Marcos'
-                })}
-              />
-            </Center>
-          </Box>
-          <Box
-            position="relative"
-            flex={1}
-            borderRadius="16px"
-            overflow="hidden"
-          >
-            <Image src="/testimonials3.jpeg" />
-            <Center
-              position="absolute"
-              top="0"
-              left="0"
-              w="100%"
-              h="100%"
-              zIndex="200"
-            >
-              <Image
-                src="/icon-play.svg"
-                w="96px"
-                h="96px"
-                _hover={{ opacity: "0.6" }}
-                cursor="pointer"
-                onClick={() => setVideoModalConfig({
-                  isOpen: true,
-                  videoId: 'gbgnLODrEkU',
-                  title: 'Depoimento Kleber'
-                })}
-              />
-            </Center>
-          </Box>
-        </Stack>
+        <HStack mt="10" spacing={{base: 2, md: 4}} h="100%" overflow={{base: 'scroll' , md: 'auto' }}>
+          <TestimonialBox
+            image="/testimonials1.jpeg"
+            handleClick={() => setVideoModalConfig({
+              isOpen: true,
+              videoId: '9qK8lmLd3w4',
+              title: 'Depoimento Jurandir'
+            })}
+          />
+          <TestimonialBox
+            image="/testimonials2.jpeg"
+            handleClick={() => setVideoModalConfig({
+              isOpen: true,
+              videoId: 'y7_B4kCI93o',
+              title: 'Depoimento Marcos'
+            })}
+          />
+          <TestimonialBox
+            image="/testimonials3.jpeg"
+            handleClick={() => setVideoModalConfig({
+              isOpen: true,
+              videoId: 'gbgnLODrEkU',
+              title: 'Depoimento Kleber'
+            })}
+          />
+        </HStack>
       </Container>
     </Section>
   );
