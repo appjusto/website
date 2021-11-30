95e440147246a1694ea7c5cbdaa4048d120fe578
RenanC <renancostam@gmail.com>
2021-11-30 11:53:09 -0300

    Add Seo to restaurants calculator

--- frontend/src/pages/calculadoras/restaurantes.tsx
+++ frontend/src/pages/calculadoras/restaurantes.tsx
@@ -2,13 +2,25 @@ import PageLayout from '../../components/PageLayout'
 import RestaurantsHero from '../../components/calculators/restaurants/hero'
 import SharingModal from '../../components/SharingModal'
 import RestaurantCalculator from '../../components/calculators/restaurants/calculator'
+import Head from "next/head";
+import Seo from '../../components/Seo';
 
 export default function Home() {
   return (
-    <PageLayout pageName="Restaurantes">
-      <RestaurantsHero />
-      <RestaurantCalculator />
-      <SharingModal />
-    </PageLayout>
+    <>
+      <Head>
+        <Seo
+          metaDescription="Mais do que um app de entregas. Um movimento por relações mais justas e transparentes para restaurantes, entregadores e clientes. Faça parte desse movimento!"
+          title="AppJusto"
+          author="@appjusto"
+          canonical_url="https://appjusto.com.br/"
+        />
+      </Head>
+      <PageLayout pageName="Restaurantes">
+        <RestaurantsHero />
+        <RestaurantCalculator />
+        <SharingModal />
+      </PageLayout>
+    </>
   )
 }
