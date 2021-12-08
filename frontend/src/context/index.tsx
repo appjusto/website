35fc63aa9cb0672fdc58a32833573d011cc21e11
RenanC <renancostam@gmail.com>
2021-12-08 11:02:43 -0300

    Update kriaLink

--- frontend/src/context/index.tsx
+++ frontend/src/context/index.tsx
@@ -47,7 +47,7 @@ export const PageContextProvider = (props: Props) => {
   const env = process.env.NEXT_PUBLIC_EXTERNAL_ENV;
   const storeLink = env === 'live' ?
     'https://login.appjusto.com.br/consumer/store' : `https://${env}.login.appjusto.com.br/consumer/store`;
-  const kriaLink = 'https://app.kria.vc/agents/users/offers/277/detalhes?locale=pt-BR';
+  const kriaLink = 'https://app.kria.vc/agents/users/offers/277?locale=pt-BR';
   // side effects
   /*React.useEffect(() => {
     (async () => {
