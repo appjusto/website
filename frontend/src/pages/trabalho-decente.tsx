1de34c982efdc290485a31d6a81a2a1c5d2ebd30
Renan Costa <renancostam@gmail.com>
2023-04-27 16:53:14 -0300

    DecentWorkPage hero structure

--- frontend/src/pages/trabalho-decente.tsx
+++ frontend/src/pages/trabalho-decente.tsx
@@ -1,7 +1,9 @@
 import PageLayout from "../components/PageLayout";
 import Head from "next/head";
 import Seo from "../components/Seo";
-import { Box, Flex, Heading, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image } from "@chakra-ui/react";
+import Section from "src/components/Section";
+import Container from "src/components/Container";
 
 export default function DecentWorkPage() {
   return (
@@ -14,18 +16,48 @@ export default function DecentWorkPage() {
         />
       </Head>
       <PageLayout pageName="Trabalho decente">
-        <Flex>
-          <Box>
-            <Heading as="h1">AppJusto na luta pelo trabalho decente</Heading>
-            <Text>
-              O AppJusto, como forma de aprimorar seu combate à precarização do
-              trabalho pelas plataformas, está trabalhando na luta pelo trabalho
-              decente. Elencamos as ações que estamos fazendo e que ainda iremos
-              fazer baseada nos seguintes critérios: remuneração, contratos,
-              gestão, condições e representações justas.
-            </Text>
-          </Box>
-        </Flex>
+        <Section id="hero" overflow="hidden">
+          <Container maxW="1040px">
+            <Flex
+              mt={{ base: "60px", md: "200px", lg: "120px" }}
+              justifyContent="space-between"
+            >
+              <Box maxW={{ base: "100%", md: "600px", lg: "740px" }}>
+                <Heading
+                  as="h1"
+                  maxW={{ base: "311px", md: "480px", lg: "648px" }}
+                  fontSize={{ base: "4xl", md: "5xl", lg: "6xl" }}
+                  fontWeight="700"
+                  lineHeight={{ base: "2.6rem", md: "3.5rem", lg: "4.75rem" }}
+                >
+                  AppJusto na luta pelo trabalho decente
+                </Heading>
+                <Heading
+                  mt="8"
+                  as="h2"
+                  maxW="648px"
+                  fontSize="xl"
+                  lineHeight="1.62rem"
+                  fontWeight="500"
+                >
+                  O AppJusto, como forma de aprimorar seu combate à precarização
+                  do trabalho pelas plataformas, está trabalhando na luta pelo
+                  trabalho decente. Elencamos as ações que estamos fazendo e que
+                  ainda iremos fazer baseada nos seguintes critérios:
+                  remuneração, contratos, gestão, condições e representações
+                  justas.
+                </Heading>
+              </Box>
+              <Box pl="8">
+                <Image
+                  src="/app_courier.jpg"
+                  alt="tela do app de entregadores"
+                  width="320px"
+                />
+              </Box>
+            </Flex>
+          </Container>
+        </Section>
       </PageLayout>
     </>
   );
